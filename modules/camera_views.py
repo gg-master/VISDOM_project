35b79ff429629afcc052c0f2786fbee8e3694a8d
gg-master <korshun.tolik@yandex.ru>
2021-10-21 19:47:50 +0300

    Отключил вывод ошибки в camera_views.py 100

--- modules/camera_views.py
+++ modules/camera_views.py
@@ -97,7 +97,7 @@ class MainWindowCamera(QThread):
                     # Добавляем новые координаты для точки определенного цвета
                     self.parent().analyzer.add_next_position(name, (x, y))
                 except Exception as e:
-                    print('camera_views.py:88 // exp //', e)
+                    # print('camera_views.py:100 // exp //', e)
                     pass
 
                 cv2.circle(img, (x, y), 5, color_yellow, 2)
