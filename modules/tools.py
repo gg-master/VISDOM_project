644f4ec2ed79442872469ca1e85022c892d56909
gg-master <korshun.tolik@yandex.ru>
2022-03-16 23:34:16 +0300

    Подправил пути

--- modules/tools.py
+++ modules/tools.py
@@ -1,8 +1,14 @@
 import os
+import sys
 
 
 def abspath(rel_path: str) -> str:
     rel_path = '/'.join(rel_path.split('\\'))
     if os.getcwd().endswith('modules'):
         os.chdir('..')
-    return rel_path
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, rel_path)
